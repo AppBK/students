dcb5644d74f16a37e2dccf12224c072df0b1441d
AppBK <app_academy_student_bk@protonmail.com>
2023-08-10 14:02:45 -0700

    cleared

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-from .user import User
-from .film import Film
-from .person import Person
-from .review import Review
-from .role import Role
-from .seen_film import SeenFilm
-from .db import environment, SCHEMA
